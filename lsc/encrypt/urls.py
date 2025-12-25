f584547ae7f84741268366fc581101aa02212e30
martinthiriet <martin.thiriet1@gmail.com>
2025-12-25 01:02:01 +0100

    Delete lsc/encrypt directory

--- lsc/encrypt/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.home_encrypt, name="home_encrypt"),
-    path("source/",views.source_code,name="source_code")
-]
